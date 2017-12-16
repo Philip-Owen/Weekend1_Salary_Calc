36bf38e45a4f133b1d3a19eb2621ed0781a7e28c
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2017-12-16 17:40:01 -0600

    Created function to append information being taken in from the input fields, on to the DOM.

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ $(document).ready(onReady);
 
 // Runs when DOM is ready
 function onReady() {
-  $('#submitEmployee').on('click', newEmployeeInfo);
+  $('#submitEmployee').on('click', addNewEmployee);
 } // end onReady() function
 
 // Gets info out of input fields and returns an array of the data
@@ -18,3 +18,16 @@ function newEmployeeInfo() {
     employeeTitle, employeeSal];
   return employeeInfoArray;
 } // end newEmployeeInfo()
+
+// Adds new employee information to the DOM and updates monthy cost calculations
+function addNewEmployee() {
+  employeeData = newEmployeeInfo();
+  console.log(employeeData);
+  var $row = $('<tr>');
+  $row.append('<td>'+ employeeData[0] + '</td>');
+  $row.append('<td>'+ employeeData[1] + '</td>');
+  $row.append('<td>'+ employeeData[2] + '</td>');
+  $row.append('<td>'+ employeeData[3] + '</td>');
+  $row.append('<td>'+ employeeData[4] + '</td>');
+  $('#employeeData').append($row);
+} // end addNewEmployee()
