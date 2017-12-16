a7778c6121bd454806bb37953e35dc75b4938f00
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2017-12-16 17:26:29 -0600

    Added JS function to retrieve employee information from input fields.

--- client.js
+++ client.js
@@ -2,6 +2,18 @@ console.log('js');
 
 $(document).ready(onReady);
 
+// Runs when DOM is ready
 function onReady() {
-  console.log('jq');
-}
+  $('#submitEmployee').on('click', newEmployeeInfo);
+} // end onReady() function
+
+// Gets info out of input fields and returns an array of the data
+function newEmployeeInfo() {
+  var empFName = $('#firstName').val();
+  var empLName = $('#lastName').val();
+  var empID = $('#empID').val();
+  var empJobTitle = $('#jobTitle').val();
+  var empSal = Number($('#annualSalary').val());
+  var empInfoArray = [empFName, empLName, empID, empJobTitle, empSal];
+  return empInfoArray;
+} // end newEmployeeInfo()
