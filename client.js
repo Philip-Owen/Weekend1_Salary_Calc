72dc04a90cc56f239013fe14a0fc479a2617ccf2
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2017-12-16 18:37:15 -0600

    Added comments to functions in JS file. Completed Base mode.

--- client.js
+++ client.js
@@ -21,28 +21,45 @@ function newEmployeeInfo() {
 
 // Adds new employee information to the DOM and updates monthy cost calculations
 function addNewEmployee() {
-  employeeData = newEmployeeInfo();
-  console.log(employeeData);
+  // Runs newEmployeeInfo() function to retrieve info from input fields so
+  // that the data can be appended to the DOM
+  var employeeData = newEmployeeInfo();
+
   var $row = $('<tr>');
   $row.append('<td>'+ employeeData[0] + '</td>');
   $row.append('<td>'+ employeeData[1] + '</td>');
   $row.append('<td>'+ employeeData[2] + '</td>');
   $row.append('<td>'+ employeeData[3] + '</td>');
   $row.append('<td>'+ employeeData[4] + '</td>');
+
+  // adds a data tag to each row containing the employees salary
   $row.data('salary', employeeData[4]);
+  // Appends values from the employeeData array to the DOM
   $('#employeeData').append($row);
+  // Clears input boxes after submission
   $('input').not('#submitEmployee').val('');
+  // Runs the monthyCosts() function each time a new row is created to get accurate
+  // calculations of the current montly costs.
   var totalMontlyCost = monthyCosts();
-  $('#salTotal').html(totalMontlyCost);
+  $('#salTotal').html(totalMontlyCost); // Adds monthly costs to the DOM
 } // end addNewEmployee()
+
+
 
 // Does calculations for getting the monthly salary costs.
 function monthyCosts() {
-  var addedSalaries = 0;
-  var empCount = $('tbody').children().length;
-  for (var i = 1; i <= empCount; i++) {
-    addedSalaries += $('tbody tr:nth-child(' + i + ')').data('salary');
+  var  employeeSalaryTotal = 0;
+  var employeeCount = $('tbody').children().length;
+
+  // Loops through all of the <tr>'s in the table body and get the value of
+  // data.('salary') and add that number to the variable employeeSalaryTotal
+  for (var i = 1; i <= employeeCount; i++) {
+    employeeSalaryTotal += $('tbody tr:nth-child(' + i + ')').data('salary');
   }
-  addedSalaries = Number((addedSalaries / 12).toFixed(2));
-  return addedSalaries;
+
+  // Takes output of the loop and divides the number by 12 to give monthly total.
+  // Uses toFixed(2) so that the number will only have two places after the
+  // decimal and then returns that value to a number.
+  var monthlySalaryTotal = Number((employeeSalaryTotal / 12).toFixed(2));
+  return monthlySalaryTotal;
 } // end monthyCosts()
